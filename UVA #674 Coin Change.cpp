523cf0a9c00f8c2d289838c4ae2f50c33015cb90
Omar Rehan <omar.se.rehan@gmail.com>
2018-10-07 10:10:17 +0200

    Update UVA #674 Coin Change.cpp

--- UVA #674 Coin Change.cpp
+++ UVA #674 Coin Change.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 
 int main(){
-    int k, coins[5];
-    coins[0] = 1, coins[1] = 5, coins[2] = 10, coins[3] = 25, coins[4] = 50;
+    int k, coins[5] = {1, 5, 10, 25, 50};
     int dp[5][7500] = {0};
-    for (int i = 0; i < 5; i++) dp[i][0] = 1;
-    for (int trgt = 1; trgt < 7500; trgt++){
-        for (int idx = 0; idx < 5; idx++){
+    for (int i = 0; i < 5; i++) dp[i][0] = 1; // base case
+    
+    for (int trgt = 1; trgt < 7500; trgt++){ // all possible amounts
+        for (int idx = 0; idx < 5; idx++){ // all types of coins
             if (idx) dp[idx][trgt] = dp[idx-1][trgt];
             if (trgt >= coins[idx]) dp[idx][trgt] += dp[idx][trgt-coins[idx]];
         }
     }
+    
     while ((scanf("%d", &k)) != EOF){
         printf("%d\n", dp[4][k]);
     }
